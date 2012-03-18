972b4d9383d0bfd1d1aa4996cf7d468c226cca28
Michael Ritter <ritter.michael92@gmail.com>
2012-03-18 23:20:09 +0100

    rewriting the listener

--- src/me/kantenkugel/serveress/whitelisted/WlListener.java
+++ src/me/kantenkugel/serveress/whitelisted/WlListener.java
@@ -1,6 +1,8 @@
 package me.kantenkugel.serveress.whitelisted;
 
 import java.util.HashMap;
+
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -25,15 +27,21 @@ public class WlListener implements Listener {
 				if(plugin.showconsolelog) plugin.logger.info(plugin.chatprefix + "...deny!");							//deny debug output
 				if(plugin.notify && plugin.denylist.contains(pl.getName().toLowerCase()) == false) {					//notify enabled and player not denied
 					if(queuehash.containsKey(pl.getName().toLowerCase())) {												//if player tried before
-						if(queuehash.get(pl.getName().toLowerCase()) < (System.currentTimeMillis() + 20*1000)) {		//if he tried at least 20secs before
+						if(queuehash.get(pl.getName().toLowerCase()) < (System.currentTimeMillis() - 20*1000)) {		//if he tried at least 20secs before
 							queuehash.remove(pl.getName().toLowerCase());												//delete from list, treat like first time
 						} else {																						//if recent try
 							event.disallow(Result.KICK_WHITELIST, "no admin has approved you yet");						//not approved yet
 							return;																						//block rest of function
 						}
 					}
-					int num =plugin.getServer().broadcast("Player "+pl.getName()+" is trying to join... accept/deny?", "whitelisted.admin");	//broadcast
-					if(num == 0) {																												//no admin reached
+					boolean found=false;
+					for(Player online: plugin.getServer().getOnlinePlayers()) {											//search for admins and broadcast
+						if(online.hasPermission("whitelisted.admin")) {
+							online.sendMessage(ChatColor.YELLOW+plugin.chatprefix+"Player "+pl.getName()+" is trying to join... add/deny?");
+							found=true;
+						}
+					}
+					if(!found) {																												//no admin reached
 						event.disallow(Result.KICK_WHITELIST, "Your not whitelisted and no admin online");
 					} else {																												//admin reached
 						queuehash.put(pl.getName().toLowerCase(), System.currentTimeMillis());												//add to list
